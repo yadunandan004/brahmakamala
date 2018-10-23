b69b2a16f445bae4334284316d073d827061a96a
Yadu_Nandan <yadunandan4992@gmail.com>
2018-10-22 23:08:07 -0700

    fixed path resolve err

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 80
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 80;
+var path    = require("path");
 
-app.get('/', (req, res) => res.sendFile('static/index.html'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/static/index.html'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

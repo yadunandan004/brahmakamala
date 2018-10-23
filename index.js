414bc6b6dcd18cdd9b0dccb4135f9b585b5ca407
Yadu_Nandan <yadunandan4992@gmail.com>
2018-10-22 23:05:28 -0700

    render html page

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 80
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.sendFile('static/index.html'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

a63330de49a6eaab8c416dd03c45eb2096a7f710
Yadu_Nandan <yadunandan4992@gmail.com>
2018-10-22 23:51:49 -0700

    added nav bar

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const port = process.env.PORT || 80;
 var path    = require("path");
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/static/index.html')));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
